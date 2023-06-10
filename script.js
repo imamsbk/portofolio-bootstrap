2ef0ffad618b7b9531e59bd38ac09dfe2b3ae08b
Imam Subakir <imam.subakir21@gmail.com>
2023-06-10 17:57:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNavbar() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
